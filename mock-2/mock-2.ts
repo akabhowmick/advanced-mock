03a67bf729b9d5327aa0d7e267e2579d081309a7
Akash <akabhowmick@gmail.com>
2025-03-26 11:15:56 -0400

    solve mock 2 problem 1

--- mock-2/mock-2.ts
+++ mock-2/mock-2.ts
@@ -7,6 +7,11 @@
 // # Output: True
 // # Input: 10
 // # Output: False
+function isPowerOfThree(n: number): boolean {
+  if (n === 1) return true;  
+  if (n <= 0 || n % 3 !== 0) return false; 
+  return isPowerOfThree(n / 3); 
+}
 
 // # (Medium) Combination Sum
 // # Given an array of unique integers and a target sum, return all unique combinations where numbers sum to the target.
